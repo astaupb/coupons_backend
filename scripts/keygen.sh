cb15c4ba980d3ee2eb44429e4b3fa8dd3bb25831
Leon Tappe <ltappe@mail.uni-paderborn.de>
2020-11-04 15:02:06 +0100

    final fix for keygen
    all hail to carriage return

--- scripts/keygen.sh
+++ scripts/keygen.sh
@@ -6,12 +6,10 @@ echo "Generating key batch '${1}' with ${2} keys"
 
 mkdir -p codes
 
-rm -f codes/$1.csv
-
-echo \"id\",\"code\" >> codes/$1.csv
+echo \"id\",\"code\"$'\r' > codes/$1.csv
 
 for (( i=1; i <= $2; i++ ))
 do
-	echo \"$1.$i\",\"`uuidgen`\" >> codes/$1.csv
+	echo \"$1.$i\",\"`uuidgen`\"$'\r' >> codes/$1.csv
 done
 
